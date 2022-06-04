3d1192289a0d985b660da93f9be214d22b820a02
Darya S <dashka.sab@gmail.com>
2022-06-04 13:29:50 +0200

    feat: add Chilp process implementation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const ls = spawn('node', ['files/script.js', ...args]);
+    process.stdin.pipe(ls.stdin);
+
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
 };
+
+spawnChildProcess(process.argv.slice(2))
