414f2541d02347216c30d1356b1c17ecc1392a61
Darya S <dashka.sab@gmail.com>
2022-06-04 15:35:49 +0200

    feat: add Worker Threads implementation

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const creator = (i) => new Promise((resolve, reject) => {
+    const worker = new Worker('./worker.js', {
+        workerData: 10 + i
+    });
+    worker.on('message', (res) => resolve(res));
+    worker.on('error', reject);
+});
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuData = os.cpus();
+    const arrResults = [];
+
+    for (let i = 0; i < cpuData.length; i++) {
+        try {
+            const data = await creator(i);
+            arrResults.push(
+                {
+                    status: 'resolved',
+                    data: data
+                }
+            );
+
+        } catch {
+            arrResults.push(
+                {
+                    status: 'error',
+                    data: null
+                }
+            );
+        }
+    }
+
+    return arrResults;
 };
+
+performCalculations();
