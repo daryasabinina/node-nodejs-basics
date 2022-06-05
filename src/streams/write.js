f4265a75299af07db6eab15ef50474e18b7ffcc1
Darya S <dashka.sab@gmail.com>
2022-06-05 11:10:32 +0200

    fix: remove exit just in case

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
-import { stdin, exit } from 'process';
+import { stdin } from 'process';
 
 export const write = async () => {
     const file = new URL('files/fileToWrite.txt', import.meta.url);
     const stream = fs.createWriteStream(file);
     stdin.on('data', data => {
         stream.write(data.toString());
-        exit();
     });
 };
 
