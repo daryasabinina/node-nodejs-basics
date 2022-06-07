3addb5209415161ea62d0790741caf820983dd41
Darya S <dashka.sab@gmail.com>
2022-06-07 23:06:05 +0200

    refactor: rewrite has to try catch

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -5,11 +5,12 @@ export const calculateHash = async () => {
     const file = new URL('files/fileToCalculateHashFor.txt', import.meta.url);
     const hash = crypto.createHash('sha256');
 
-    fs.readFile(file)
-        .then(res => {
-            hash.update(res);
-            console.log(hash.digest('hex'));
-        })
+    try {
+        const content = await fs.readFile(file);
+        console.log(hash.update(content).digest('hex'));
+    } catch (err) {
+        throw err;
+    }
 };
 
 calculateHash();
