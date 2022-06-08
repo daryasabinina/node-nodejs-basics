3c63ce7fba73291f0e2869c19757b1d53787317b
Darya S <dashka.sab@gmail.com>
2022-06-08 15:13:55 +0200

    refactor: improvements for wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,7 +3,7 @@ import { workerData, parentPort } from 'worker_threads';
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    parentPort.postMessage(nthFibonacci(workerData));
+    parentPort.postMessage(nthFibonacci(workerData)); // add error and check
     process.exit();
 };
 
